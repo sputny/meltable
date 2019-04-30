a3941e3c6669875f62192e6f854847422704ede2
tobi <tobias_huttner@mailbox.org>
2019-04-30 23:38:52 +0200

    add docopt interface and cvs write possiblity

--- meltable/meltable.py
+++ meltable/meltable.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 # coding=<utf-8>
+"""
+usage:
+    meltable [-v -o]  <file> ...
+
+options:
+    -v      Be verbose.
+    -o      Override existing files.
+"""
 import PyPDF2 as pdf
-import argparse
+import docopt
 import re
+import os
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-            '-o', tpye=argparse.FileType('w'),
-            help="name of output file")
+    args = docopt.docopt(__doc__)
+    team, athlets = parse_pdf(args)
+    team_filename = "team.csv"
+    athlet_filename = "athlet.csv"
+    if args['-o'] or not(
+            os.path.exists(team_filename) or os.path.exists(athlet_filename)):
+        save_csv(team_filename, team)
+        save_csv(athlet_filename, athlets)
 
 
-def parse_pdf(filenames):
-    if isinstance(filenames, str):
-        filenames = [filenames]
+def parse_pdf(args):
+    filenames = args['<file>']
     team = []
     athlets = []
     team_regex = re.compile(r"^u(\d)$")
@@ -28,10 +40,10 @@ def parse_pdf(filenames):
             var = [
                 text['vereinsnr'], text['u' + t.group(1)],
                 text['t' + t.group(1)], text['tn' + t.group(1)], '']
-            if all([x == '' for x in var[1:4]]):
+            if args['-v'] and all([x == '' for x in var[1:4]]):
                 print("Auf Meldung {} ist Team {} nicht befüllt".format(
                     filename, n))
-            elif any([x == '' for x in var[1:4]]):
+            elif args['-v'] and any([x == '' for x in var[1:4]]):
                 print("Auf Meldung {} ist Team {}"
                       + " nicht vollständing befüllt".format(filename, n))
             else:
@@ -46,12 +58,19 @@ def parse_pdf(filenames):
                         text['vn' + a.group(1) + a.group(2)],
                         text['jg' + a.group(1) + a.group(2)],
                         text['vereinsnr'], text['tn' + a.group(1)]]
-                if all([x == '' for x in a_var[:3]]):
-                    print("Auf Meldung {} Teilnehme {} "
-                          + "von Team {} nicht befüllt".format(filename, k, n))
-                elif any([x == '' for x in a_var[:3]]):
-                    print("Auf Meldung {} Teilnehme {} von Team {} nicht"
-                          + " vollständing befüllt".format(filename, k, n))
+                if args['-v'] and all([x == '' for x in a_var[:3]]):
+                    print("Auf Meldung {} Teilnehmer {} ".format(filename, k)
+                          + "von Team {} nicht befüllt".format(n + 1))
+                elif args['-v'] and any([x == '' for x in a_var[:3]]):
+                    print("Auf Meldung "
+                            + "{} Teilnehmer {} von Team {} nicht".format(
+                                filename, k, n)
+                            + " vollständing befüllt")
                 else:
                     athlets.append(a_var)
     return team, athlets
+
+
+def save_csv(name, content):
+    with open(name, 'w') as f:
+        [f.writeline(";".join(x)+ "\n") for x in content]
--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     name="meltable",
     version="0.1",
     packages=find_packages(),
-    install_requires=['PyPDF2'],
-    author="sputny",
+    install_requires=['PyPDF2', 'docopt'],
+    author="thuttner",
     license="GPL2",
     entry_points={
         'console_scripts': [
-            'meltable = meltable.main',
+            'meltable = meltable.meltable:main',
             ]
         },
     )

06cc1bc8162f13aff93dda577949d47fbb5f7e01
tobi <tobias_huttner@mailbox.org>
2019-05-01 13:02:15 +0200

    use csv stdlib module for csv generation
    fix bug in verbosity checks

--- meltable/meltable.py
+++ meltable/meltable.py
@@ -12,6 +12,7 @@ import PyPDF2 as pdf
 import docopt
 import re
 import os
+import csv
 
 
 def main():
@@ -23,6 +24,9 @@ def main():
             os.path.exists(team_filename) or os.path.exists(athlet_filename)):
         save_csv(team_filename, team)
         save_csv(athlet_filename, athlets)
+    else:
+        print("{} or {} already exist. Provide -o switch for override.".format(
+            team_filename, athlet_filename))
 
 
 def parse_pdf(args):
@@ -40,12 +44,14 @@ def parse_pdf(args):
             var = [
                 text['vereinsnr'], text['u' + t.group(1)],
                 text['t' + t.group(1)], text['tn' + t.group(1)], '']
-            if args['-v'] and all([x == '' for x in var[1:4]]):
-                print("Auf Meldung {} ist Team {} nicht befüllt".format(
-                    filename, n))
-            elif args['-v'] and any([x == '' for x in var[1:4]]):
-                print("Auf Meldung {} ist Team {}"
-                      + " nicht vollständing befüllt".format(filename, n))
+            if all([x == '' for x in var[1:4]]):
+                if args['-v']:
+                    print("Auf Meldung {} ist Team {} nicht befüllt".format(
+                        filename, n))
+            elif any([x == '' for x in var[1:4]]):
+                if args['-v']:
+                    print("Auf Meldung {} ist Team {}"
+                          + " nicht vollständing befüllt".format(filename, n))
             else:
                 team.append(var)
             for k, a in enumerate([
@@ -58,19 +64,23 @@ def parse_pdf(args):
                         text['vn' + a.group(1) + a.group(2)],
                         text['jg' + a.group(1) + a.group(2)],
                         text['vereinsnr'], text['tn' + a.group(1)]]
-                if args['-v'] and all([x == '' for x in a_var[:3]]):
-                    print("Auf Meldung {} Teilnehmer {} ".format(filename, k)
+                if all([x == '' for x in a_var[:3]]):
+                    if args['-v']:
+                        print("Auf Meldung {} Teilnehmer {} ".format(
+                            filename, k)
                           + "von Team {} nicht befüllt".format(n + 1))
-                elif args['-v'] and any([x == '' for x in a_var[:3]]):
-                    print("Auf Meldung "
-                            + "{} Teilnehmer {} von Team {} nicht".format(
-                                filename, k, n)
-                            + " vollständing befüllt")
+                elif any([x == '' for x in a_var[:3]]):
+                    if args['-v']:
+                        print("Auf Meldung "
+                              + "{} Teilnehmer {} von Team {} nicht".format(
+                                  filename, k, n)
+                              + " vollständing befüllt")
                 else:
                     athlets.append(a_var)
     return team, athlets
 
 
 def save_csv(name, content):
-    with open(name, 'w') as f:
-        [f.writeline(";".join(x)+ "\n") for x in content]
+    with open(name, 'w', newline='') as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerows(content)
